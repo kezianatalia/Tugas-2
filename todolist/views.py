629f2a262f6de0480fbbd8b16986fd5f0ff77f1f
kezia.natalia <kezianatalia1812@gmail.com>
2022-09-28 19:23:17 +0700

    sort tabel todo

--- todolist/views.py
+++ todolist/views.py
@@ -13,7 +13,7 @@ from django.urls import reverse
 # TODO: Create your views here.
 @login_required(login_url='/todolist/login/')
 def show_todolist(request):
-    data_todolist = Task.objects.all()
+    data_todolist = Task.objects.all().order_by('date')
     context = {
         'list_todo' : data_todolist,
     }
